178daa41a728a24033bcd324c8eebebe77dcb824
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-01-24 23:30:12 +0530

    Added post method and pug code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//multer object creation
+var multer  = require('multer')
+var upload = multer({ dest: 'public/uploads/' })
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', upload.any(),function(req, res) {
+  res.send("File upload sucessfully.");
 });
 
 module.exports = router;
