68a598b1042907b90496bf983465afbb3ac23d1d
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-01-24 23:43:26 +0530

    File upload name related changes.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,23 @@ var router = express.Router();
 
 //multer object creation
 var multer  = require('multer')
-var upload = multer({ dest: 'public/uploads/' })
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', upload.any(),function(req, res) {
+router.post('/', upload.single('imageupload'),function(req, res) {
   res.send("File upload sucessfully.");
 });
 
